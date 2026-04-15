2d2d3f8e7a76efa7799119c82a5339ab814eae9f
jkavanagh00 <jkavanagh00@gmail.com>
2026-04-15 11:49:57 +0200

    add create_booking_table in line with ERD v1

---
+++ api/src/db/migrations/20260414215500_create_booking_table.js
@@ -0,0 +1,19 @@
+/**
+ * @param {import("knex").Knex} knex
+ */
+export async function up(knex) {
+    await knex.schema.createTable("booking", (t) => {
+        t.increments("id").primary();
+        t.integer("user_id").notNullable().references("id").inTable("user");
+        t.integer("event_id").notNullable().references("id").inTable("event");
+        t.string("status").notNullable();
+        t.timestamp("created_at").notNullable();
+    });
+}
+
+/**
+ * @param {import("knex").Knex} knex
+ */
+export async function down(knex) {
+    await knex.schema.dropTableIfExists("booking");
+}
