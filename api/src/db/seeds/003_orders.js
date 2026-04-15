707c8470e1310a6fdb1672d0bad5a75aa212e7e4
jkavanagh00 <jkavanagh00@gmail.com>
2026-04-15 17:48:19 +0200

    add 003_orders.js seed file

---
+++ api/src/db/seeds/003_orders.js
@@ -0,0 +1,60 @@
+/**
+ * @param {import("knex").Knex} knex
+ */
+export async function seed(knex) {
+  await knex("order").del();
+
+  await knex("order")
+    .insert([
+      {
+        id: 1,
+        account_id: 3,
+        event_id: 1,
+        status: "pending"
+      },
+      {
+        id: 2,
+        account_id: 1,
+        event_id: 2,
+        status: "confirmed"
+      },
+      {
+        id: 3,
+        account_id: 2,
+        event_id: 3,
+        status: "cancelled"
+      },
+      {
+        id: 4,
+        account_id: 4,
+        event_id: 4,
+        status: "pending"
+      },
+      {
+        id: 5,
+        account_id: 5,
+        event_id: 5,
+        status: "confirmed"
+      },
+      {
+        id: 6,
+        account_id: 6,
+        event_id: 6,
+        status: "cancelled"
+      },
+      {
+        id: 7,
+        account_id: 7,
+        event_id: 7,
+        status: "pending"
+      },
+      {
+        id: 8,
+        account_id: 8,
+        event_id: 8,
+        status: "confirmed"
+      },
+    ])
+    .onConflict("id")
+    .merge();
+}
