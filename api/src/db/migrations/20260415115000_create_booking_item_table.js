582699e82899c22a90ace5b4803420b67f0517c9
jkavanagh00 <jkavanagh00@gmail.com>
2026-04-22 12:23:35 +0200

    refactor create_booking_table and create_booking_item_table to refer to booking instead of order

--- api/src/db/migrations/20260415115000_create_booking_item_table.js
+++ api/src/db/migrations/20260415115000_create_booking_item_table.js
@@ -2,7 +2,7 @@
  * @param {import("knex").Knex} knex
  */
 export async function up(knex) {
-    await knex.schema.createTable("order_item", (t) => {
+    await knex.schema.createTable("booking_item", (t) => {
         t.increments("id").primary();
         t.integer("order_id").notNullable().references("id").inTable("order");
         t.integer("event_id").notNullable().references("id").inTable("event");
@@ -16,5 +16,5 @@ export async function up(knex) {
  * @param {import("knex").Knex} knex
  */
 export async function down(knex) {
-    await knex.schema.dropTableIfExists("order_item");
+    await knex.schema.dropTableIfExists("booking_item");
 }
