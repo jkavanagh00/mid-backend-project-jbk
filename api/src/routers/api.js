5cbe219b6b0f069ab4340761efe78d06dfa6fdd4
jkavanagh00 <jkavanagh00@gmail.com>
2026-05-07 14:40:39 +0200

    added carts router to api.js

--- api/src/routers/api.js
+++ api/src/routers/api.js
@@ -1,10 +1,12 @@
 import express from "express";
 import eventsRouter from "#routers/events.js";
 import accountsRouter from "#routers/accounts.js";
+import cartsRouter from "#routers/carts.js";
 
 const apiRouter = express.Router();
 
 apiRouter.use("/events", eventsRouter);
 apiRouter.use("/accounts", accountsRouter);
+apiRouter.use("/cart", cartsRouter);
 
 export default apiRouter;
