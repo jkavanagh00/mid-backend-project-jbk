6e3217499f402a748c6adb8eaf232e0b0d8e42c7
jkavanagh00 <jkavanagh00@gmail.com>
2026-05-07 12:28:00 +0200

    add: identifyUserOrGuest middleware, refactor: helper functions

--- api/src/middlewares/auth.js
+++ api/src/middlewares/auth.js
@@ -1,22 +1,58 @@
 import { json } from "body-parser";
 import jsonwebtoken from "jsonwebtoken";
+import crypto from "crypto";
 
 const JWT_SECRET = process.env.JWT_SECRET;
 
 export function authenticateJWT(req, res, next) {
-    const authHeader = req.headers.authorization;
+    const token = extractToken(req);
 
-    if (!authHeader || !authHeader.startsWith("Bearer ")) {
-        return res.status(401).json({ error: "Missing or invalid Authorization header" })
-    }
-
-    const token = authHeader.split(" ")[1];
+    if (!token) return res.status(401).json({ error: "Missing or invalid Authorization header" });
 
     try {
-        const decoded = jsonwebtoken.verify(token, JWT_SECRET);
+        const decoded = verifyToken(token);
+        if (!decoded) return res.status(401).json({ error: "Invalid or expired token" });
         req.user = decoded;
         next();
     } catch (error) {
         return res.status(401).json({ error: "Invalid or expired token" });
+    }
+}
+
+export function identifyUserOrGuest(req, res, next) {
+    let token = extractToken(req);
+    if (!token) {
+        const guestId = crypto.randomUUID();
+        const guestToken = jsonwebtoken.sign({ guestId }, JWT_SECRET, { expiresIn: "7d" });
+        return res.status(200).json({ token: guestToken, guest: true });
+    }
+
+    try {
+        const decoded = verifyToken(token);
+        if (!decoded) {
+            return res.status(401).json({ error: "Invalid or expired token" });
+        }
+        req.user = decoded;
+    } catch (error) {
+        return res.status(401).json({ error: "Invalid or expired token" });
+    }
+    next();
+}
+
+// helper functions
+
+function extractToken(req) {
+    const authHeader = req.headers.authorization;
+    if (!authHeader || !authHeader.startsWith("Bearer ")) {
+        return null;
+    }
+    return authHeader.split(" ")[1];
+}
+
+function verifyToken(token) {
+    try {
+        return jsonwebtoken.verify(token, JWT_SECRET);
+    } catch (error) {
+        return null;
     }
 }
