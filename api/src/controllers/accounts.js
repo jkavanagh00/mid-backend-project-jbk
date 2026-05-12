b1a11f9077864fe84c9659ba587e42e8b855cee0
jkavanagh00 <jkavanagh00@gmail.com>
2026-05-12 15:12:38 +0200

    fix bug causing unhashed passwords to be saved on PATCH /accounts

--- api/src/controllers/accounts.js
+++ api/src/controllers/accounts.js
@@ -12,6 +12,7 @@ import {
   AccountInput,
   AccountPatchInput,
 } from "#schemas/accounts.js";
+import bcrypt from "bcrypt";
 
 export async function getAccounts(req, res, next) {
   try {
@@ -67,11 +68,13 @@ export async function getAccountById(req, res, next) {
 }
 
 export async function patchAccount(req, res, next) {
-  // OPTIONAL TODO: implement this handler only if optional scope is taken on
   try {
-    // Validate both the route params and the partial PATCH payload.
     const { id } = AccountIdParams.parse(req.params);
     const accountPatchInput = AccountPatchInput.parse(req.body);
+    if (accountPatchInput.password) {
+      const hashedPassword = await bcrypt.hash(accountPatchInput.password, 10);
+      accountPatchInput.password = hashedPassword;
+    }
 
     const updatedAccount = await updateAccount(id, accountPatchInput);
 
