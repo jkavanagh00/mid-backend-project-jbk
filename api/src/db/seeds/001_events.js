cefa26b2d8812e2374b7cf478e5aa91e8e4214cd
jkavanagh00 <jkavanagh00@gmail.com>
2026-05-23 23:09:37 +0200

    clear dependent records

--- api/src/db/seeds/001_events.js
+++ api/src/db/seeds/001_events.js
@@ -2,12 +2,12 @@
  * @param {import("knex").Knex} knex
  */
 export async function seed(knex) {
-  await knex('cart_items').del();
-  await knex('booking_items').del();
-  await knex('bookings').del();
-  await knex('carts').del();
-  await knex('accounts').del();
-  await knex('events').del();
+  await knex('cart_item').del();
+  await knex('booking_item').del();
+  await knex('booking').del();
+  await knex('cart').del();
+  await knex('account').del();
+  await knex('event').del();
 
   await knex("event").insert([
     {
